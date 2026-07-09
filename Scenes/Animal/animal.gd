94a18905f423a8214f2c87cd0e591189d4773f92
vivianamo <vivianamoroochoa@gmail.com>
2026-07-09 18:00:45 -0500

    added checking for dragging logic and when you do the clicking event, it will disconnect the signal. Added arrow Sprite2D with an offset

--- Scenes/Animal/animal.gd
+++ Scenes/Animal/animal.gd
@@ -1,12 +1,21 @@
+class_name Animal
+
 extends RigidBody2D
 
 @onready var label: Label = $Label
+@onready var arrow: Sprite2D = $Arrow
+
+
+var _start: Vector2 = Vector2.ZERO
+var _drag_start: Vector2 = Vector2.ZERO
+var _is_dragging: bool = false
 
 
 func _ready() -> void:
+	_start = position	
 	#sleeping_state_changed.connect(_on_sleeping_state_changed)
 	#body_entered.connect(_on_body_entered)
-	#input_event.connect(_on_input_event)
+	input_event.connect(_on_input_event)
 	pass
 
 func _unhandled_input(event: InputEvent) -> void:
@@ -16,11 +25,17 @@ func _unhandled_input(event: InputEvent) -> void:
 		apply_central_impulse(Vector2(200, -200))
 	
 func _process(_delta: float) -> void:
-	label.text = "Freeze:%s\nContactCount:%d\nSleeping:%s" % [
+	var debug_str: String = "FR:%s CC:%d SL:%s\n" % [
 		freeze,
 		get_contact_count(),
 		sleeping
 	]
+	debug_str += "is_draggin:%s drag_start:%.0f, %.0f" % [
+		_is_dragging,
+		_drag_start.x,
+		_drag_start.y
+	]
+	label.text = debug_str
 
 func _on_sleeping_state_changed() -> void:
 	print("_on_sleeping_state_changed:", sleeping)# Replace with function body.
@@ -28,7 +43,15 @@ func _on_sleeping_state_changed() -> void:
 func _on_body_entered(body: Node) -> void:
 	print("_on_body_entered")# Replace with function body.
 
+func start_dragging() -> void:
+	arrow.show()
+	_is_dragging = true
+	_drag_start = get_global_mouse_position()
+
 
 func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
-	if event is InputEventMouseMotion and event.button_mask == 1:
-		position = get_global_mouse_position()
+	if event.is_action_pressed("drag"):
+		#to only get this event once, use disconnect
+		# it is possibly to have events automatically disconnect after one firing, by clicking into a signal and changing the advanced settings to One Shot 
+		input_event.disconnect(_on_input_event)
+		start_dragging()
