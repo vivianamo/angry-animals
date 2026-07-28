a57d5d2e6f4babb51ca0e4223e40203916b1481e
vivianamo <vivianamoroochoa@gmail.com>
2026-07-28 12:18:28 -0500

    89. 09 Cup scene complete

--- Scenes/Cup/cup.gd
+++ Scenes/Cup/cup.gd
@@ -2,5 +2,14 @@ class_name Cup
 
 extends StaticBody2D
 
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+func _ready() -> void:
+	animation_player.animation_finished.connect(_on_animation_player_animation_finished)
+
 func die() -> void:
-	pass
+	animation_player.play("vanish")
+
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	if anim_name == "vanish":
+		queue_free()
